51c3afd4024f4bb8990462ea0bd3517609a08511
Zacharie Corniere <zacharie.corniere@epitech.eu>
2019-11-30 22:16:48 +0100

    doc(crate): added mod doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-pub mod ssh_host;
 //pub mod parse_ssh_config;
+
+//! # ssh_host
+//! 
+//! `ssh_host` is a simple crate holding the nessecary value to connect to a `ssh2` host.
+//!
+//! This crate is created with a maximum compatibility with [ssh2](https://docs.rs/ssh2/0.5.0/ssh2/index.html).
+pub mod ssh_host;
 
 #[cfg(test)]
 mod tests {
